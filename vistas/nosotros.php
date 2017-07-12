0da7388624d2e328fa0675abe941a9937d7ad26f
esteban1234 <tva_libra@hotmail.com>
2017-07-12 18:11:13 -0500

    inicio de nosotros

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -1,8 +1,111 @@
 <!DOCTYPE html>
 <?php include 'encabezado2.php' ?>
 <body>
+	<header>
+  <figure class="logo">
+    <img src="../img/nokin.png" class="img-responsive" alt="">
+  </figure>
+  <div class="tch">
+    <span class="fa-stack fa-lg">
+      <i class="fa fa-circle fa-stack-2x"></i>
+      <i class="fa fa-phone fa-stack-1x fa-inverse"></i>
+    </span>
+            <span style="color: #65BFB4; font-size: 17px;">PENDIENTE</span><br>
+    <span class="fa-stack fa-lg">
+      <i class="fa fa-circle fa-stack-2x"></i>
+      <i class="fa fa-clock-o fa-stack-1x fa-inverse"></i>
+    </span>
+            <span style="color: #65BFB4; font-size: 17px;">Lunes a Viernes de 9am a 6pm</span><br>
+    <span class="fa-stack fa-lg">
+      <i class="fa fa-circle fa-stack-2x"></i>
+      <i class="fa fa-envelope-o fa-stack-1x fa-inverse"></i>
+    </span>
+            <span style="color: #65BFB4; font-size: 17px;">info@nokinservicios.com</span><br>
+  </div>
 
+  <div class="menu">
+    <ul>
+      <a class="marca" href="../index.php">INICIO</a>
+      <a class="marca select" href="nosotros.php">NOSOTROS</a>
+      <a class="marca" href="servicios.php">SERVICIOS</a>
+      <a class="marca" href="contacto.php">CONTACTO</a>
+    </ul>
+  </div>
+</header>
 
+<article class="conteninin">
+	<div class="contenininn">
+		<article class="pull-left uno">
+			<h1 style="margin-top: 5%; color: #3B3B3A; border-bottom: 1px #3B3B3A solid; padding-bottom: 1%;">Empresa</h1>
+			<p>NOKIN LOS MEJORES EN SERVICIOS S.C.P. es una empresa de servicios integrales que ofrece asesoramiento y apoyo, estando especializada en el ámbito administrativo e informático. <br><br>
+				
+				Es una empresa consolidada con años de experiencia en el sector y que pone su profundo conocimiento en las áreas de administración, procesos y tecnología al servicio de sus clientes. <br><br>
+
+				Somos expertos en el desarrollo, implementación y administración de sistemas empresariales. Te guiamos a conseguir tus objetivos implementando nuestros servicios de consultoría empresarial, análisis de negocios, cursos de capacitación, tecnología para empresas, recursos humanos, ventas, marketing y consultoría tecnológica.
+
+				Nuestras soluciones se ajustan a las necesidades de nuestros clientes ofreciendo sistemas como gestiones administrativas, manejo de plataformas, estrategias de trabajo, gestión de calidad y resultados.
+				</p>
+		</article>
+		<article class="pull-right dos">
+			<h1 style="margin-top: 5%; color: #3B3B3A; border-bottom: 1px #3B3B3A solid; padding-bottom: 1%;">Como trabajamos</h1>
+			<p>
+				<ul class="fa-ul">
+				  <li><i class="fa-li fa fa-check-square"></i>Escuchamos sus necesidades.</li>
+				  <li><i class="fa-li fa fa-check-square"></i>Le asesoramos sobre el desarrollo.</li>
+				  <li><i class="fa-li fa fa-check-square"></i>Desarrollamos los servicios siguiendo nuestros estándares de calidad</li>
+				  <li><i class="fa-li fa fa-check-square"></i>Evaluamos con el cliente el resultado final y le formamos para su utilización.</li>
+				  <li><i class="fa-li fa fa-check-square"></i>Ponemos a su disposición herramientas para el análisis del uso del servicio.</li>
+				  <li><i class="fa-li fa fa-check-square"></i>Mantenemos un cercano servicio para la conclusión de su necesidad.</li>
+				</ul>
+			</p>
+		</article>
+	</div>
+</article>
+
+<article class="mvv">
+	<div class="container" style="margin-top: 5%;">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="box">
+                <div class="box-icon">
+                    <span class="fa fa-4x fa-html5"></span>
+                </div>
+                <div class="info">
+                    <h4 class="text-center">Title</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                   
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="box">
+                <div class="box-icon">
+                    <span class="fa fa-4x fa-css3"></span>
+                </div>
+                <div class="info">
+                    <h4 class="text-center">Title</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="box">
+                <div class="box-icon">
+                    <span class="fa fa-4x fa-css3"></span>
+                </div>
+                <div class="info">
+                    <h4 class="text-center">Title</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                    
+                </div>
+            </div>
+        </div>
+	</div>
+</div>
+</article>
 
 
 <span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>

31f60b9a46393b58b0c4e744294cf5e2ddfecf03
esteban1234 <tva_libra@hotmail.com>
2017-07-12 14:46:19 -0500

    index listo

--- index.php
+++ index.php
@@ -26,16 +26,16 @@
   <div class="menu">
     <ul>
       <a class="marca select" href="index.php">INICIO</a>
-      <a class="marca" href="">NOSOTROS</a>
-      <a class="marca" href="">SERVICIOS</a>
-      <a class="marca" href="">CONTACTO</a>
+      <a class="marca" href="vistas/nosotros.php">NOSOTROS</a>
+      <a class="marca" href="vistas/servicios.php">SERVICIOS</a>
+      <a class="marca" href="vistas/contacto.php">CONTACTO</a>
     </ul>
   </div>
 </header>
 
 
 
-<section class="mbr-slider mbr-section mbr-section--no-padding carousel slide" data-ride="carousel" data-wrap="true" data-interval="5000" id="slider-5" style="background-color: #4c6972;">
+<section class="mbr-slider mbr-section mbr-section--no-padding carousel slide" data-ride="carousel" data-wrap="true" data-interval="1800" id="slider-5" style="background-color: #4c6972;">
     <div class="mbr-section__container">
         <div>
             <ol class="carousel-indicators">
@@ -44,7 +44,7 @@
                 <!-- <li data-app-prevent-settings="" data-target="#slider-5" class="" data-slide-to="2"></li> -->
             </ol>
             <div class="carousel-inner" role="listbox">
-                <div class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--bg-adapted item dark center mbr-section--full-height active" style="background-image: url(img/image1.jpg);">
+                <div class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--bg-adapted item dark center mbr-section--full-height active" style="background-image: url(img/image5.jpg);">
                     <div class="mbr-box__magnet mbr-box__magnet--center-right mbr-box__magnet--sm-padding">
 
                         <div class=" container">
@@ -57,12 +57,12 @@
 
                                     <!-- <p class="mbr-hero__subtext">Slide 2. Centered content</p> -->
                                 </div>
-                                <div class="mbr-buttons btn-inverse mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-danger" href="#">SERVICIOS</a> <a class="mbr-buttons__btn btn btn-lg btn-default" href="#">CONTÁCTANOS</a></div>
+                                <div class="mbr-buttons btn-inverse mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-danger" href="vistas/servicios.php">SERVICIOS</a> <a class="mbr-buttons__btn btn btn-lg btn-default" href="vistas/contacto.php">CONTÁCTANOS</a></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div><div class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--bg-adapted item dark center mbr-section--full-height" style="background-image: url(img/image2.jpg);">
+                </div><div class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--bg-adapted item dark center mbr-section--full-height" style="background-image: url(img/image6.jpg);">
                     <div class="mbr-box__magnet mbr-box__magnet--center-left mbr-box__magnet--sm-padding">
 
                         <div class=" container">
@@ -75,7 +75,7 @@
 
                                     <!-- <p class="mbr-hero__subtext">Slide 3. Content aligned to right </p> -->
                                 </div>
-                                <div class="mbr-buttons btn-inverse mbr-buttons--left"><a class="mbr-buttons__btn btn btn-lg btn-danger" href="#">SERVICIOS</a> <a class="mbr-buttons__btn btn btn-lg btn-default" href="#">CONTÁCTANOS</a></div>
+                                <div class="mbr-buttons btn-inverse mbr-buttons--left"><a class="mbr-buttons__btn btn btn-lg btn-danger" href="vistas/servicios.php">SERVICIOS</a> <a class="mbr-buttons__btn btn btn-lg btn-default" href="vistas/contacto.php">CONTÁCTANOS</a></div>
                                 </div>
                             </div>
                         </div>
@@ -95,7 +95,114 @@
     </div>
 </section>
 
+<div>
+  <h2 style="text-align: center; margin-top: 5%; color: #3B3B3A;">NOKIN LOS MEJORES EN SERVICIOS S.C.P</h2>
 
+  <p class="text-descript">Contamos con amplia experiencia como consultores en administración y computación implementando y desarrollando soluciones a empresas para un incremento en funcionalidad en el mundo actual y a su vez un aumento en ganancias y beneficios.</p>
+</div>
+
+<div class="contenboton">
+  <p class="contenbotontexto">Garantizamos el mejor servicio y costos. ¡Llamanos! Tel. Pendiente, o envíanos un mensaje.</p>
+  <a href="vistas/contacto.php"><button type="button" class="btn btn-danger btn-lg">Contáctanos</button></a>
+</div>
+
+
+<section class="algunos">
+  <h1 style="width: 90%; margin-left: 5%; margin-top: 5%; color: #3B3B3A; border-bottom: 1px black solid; padding-bottom: 1%;">Algunos de nuestros servicios <small>Consultoría en Administración</small></h1>
+ <br><br>
+  <div class="">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-lg-12 col-md-12">
+          <div class="row">
+            <!-- <div class="row my-5"> -->
+
+              <div class="col-xs-12 col-lg-4">
+                <div class="col-xs-12 col-lg-12"><i class="d-block fa fa-3x fa-pencil-square-o"></i></div><br><br>
+                <h4 class="new-font-tamano">AUDITORÍA DE CONTROL INTERNO</h4>
+                <p class="alinear">Control e información de la operaciones, puesto que permite el manejo adecuado de los bienes, funciones e información de una empresa determinada, con el fin de generar una indicación confiable de su situación y sus operaciones en el mercado.</p>
+              </div>
+
+            <!-- </div> -->
+            <!-- <div class="row my-5"> -->
+
+              <div class="col-xs-12 col-lg-4">
+                <div class="col-xs-12 col-lg-12"><i class="d-block fa fa-usd fa-3x"></i></div><br>
+                <h4 class="new-font-tamano">AUDITORÍA FINANCIERA</h4>
+                <p class="alinear">Examina a los estados financieros y a través de ellos las operaciones financieras realizadas por el ente contable.</p>
+              </div>
+
+            <!-- </div> -->
+            <!-- <div class="row my-5"> -->
+
+              <div class="col-xs-12 col-lg-4">
+                <div class="col-xs-12 col-lg-12"><i class="d-block fa  fa-3x fa-university"></i></div><br><br>
+                <h4 class="new-font-tamano">AUDITORÍA LEGAL</h4>
+                <p class="alinear">Obtener información sobre la cual se pueda soportar una opinión de la situación del estado legal de una empresa.</p>
+              </div>
+
+            <!-- </div> -->
+          </div>
+        </div><br><br>
+      </div>
+    </div>
+  </div>
+          
+</section>
+
+
+<section class="algunos2">
+  <h1 style="width: 90%; margin-left: 5%; margin-top: 5%; color: #3B3B3A; border-bottom: 1px black solid; padding-bottom: 1%;">Algunos de nuestros servicios <small>Consultoría en Computación</small></h1>
+ <br><br>
+  <div class="">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-lg-12 col-md-12">
+          <div class="row">
+            <!-- <div class="row my-5"> -->
+
+              <div class="col-xs-12 col-lg-4">
+                <div class="col-xs-12 col-lg-12"><i class="d-block fa fa-3x fa-pencil-square-o"></i></div><br><br>
+                <h4 class="new-font-tamano">ASESORAMIENTO WEB</h4>
+                <p class="alinear">Toda idea o proyecto debe tener un asesoramiento Web profesional.
+                Cualquier decision influye en el exito o rentabilidad del proyecto. Es importante tener presencia en Internet pero el como se haga será lo que diferencie el éxito del fracaso.
+                Desde la eleccion del dominio, el contenido de la pagina web y la tecnologia web usada son factores de gran relevancia para conseguir los objetivos propuestos a corto, medio y largo plazo.</p>
+              </div>
+
+            <!-- </div> -->
+            <!-- <div class="row my-5"> -->
+
+              <div class="col-xs-12 col-lg-4">
+                <div class="col-xs-12 col-lg-12"><i class="d-block fa fa-usd fa-3x"></i></div><br><br>
+                <h4 class="new-font-tamano">SOPORTE TECNICO</h4>
+                <p class="alinear">Todos los servicios necesarios para que los sistemas se mantengan actualizados, operando adecuadamente y adaptándose a los nuevos requerimientos del negocio. Su empresa tendrá un solo punto de contacto en donde apoyarse para levantar casos de servicio, requerir adaptaciones para nueva funcionalidad del negocio o simplemente requerir reportes especializados.</p>
+              </div>
+
+            <!-- </div> -->
+            <!-- <div class="row my-5"> -->
+
+              <div class="col-xs-12 col-lg-4">
+                <div class="col-xs-12 col-lg-12"><i class="d-block fa  fa-3x fa-university"></i></div><br><br>
+                <h4 class="new-font-tamano">Análisis, Diseño y Automatización de Procesos</h4>
+                <p class="alinear">Nuestros consultores ayudarán a su empresa a comprender mejor sus procesos de negocio y en base a un entendimiento claro, identificar las áreas de oportunidad que permitirán obtener reducciones en tiempo y costo mediante la adecuación o implementación de un sistema de información.</p>
+              </div>
+
+            <!-- </div> -->
+          </div>
+        </div><br><br>
+      </div>
+    </div>
+  </div>
+          
+</section>
+
+
+<div style="text-align: center; width: 100%;">
+  <a href="vistas/servicios.php"><button type="button" class="btn btn-danger btn-lg">Conocer todos los servicios</button></a>
+</div>
+<br><br><br><br>
 
 <span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
 <?php include 'vistas/pie.php' ?>

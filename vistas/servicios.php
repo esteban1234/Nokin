9fa7064d780681a49f6e4bc51baef9dc8f515709
esteban1234 <tva_libra@hotmail.com>
2017-07-13 18:05:40 -0500

    pagina lista

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -32,6 +32,176 @@
     </ul>
   </div>
 </header>
+
+<article class="contenservuno">
+        
+        <div class="container">
+    <div class="row">
+
+    <h1 style="width: 100%;  margin-top: 5%; color: #3B3B3A; border-bottom: 1px black solid; padding-bottom: 1%;">Servicios <small>Consultoría en Administración</small></h1> <br><br><br>
+
+    <div class="col-sm-4">
+           <div class="panel panel-blue">
+            <div class="panel-heading">Desarrollo Organizacional</div>
+            <div class="panel-body">
+            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Diseño y Revisión de la Filosofía de la Empresa. <br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Estudios sobre Factores Clave que inciden en el &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;desempeño del personal (Clima, Satisfacción, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rotación, etc.).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Plan de Vida y Carrera.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Tabulador de Sueldos y Salarios.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Reingeniería.
+			</div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+           <div class="panel panel-pink">
+            <div class="panel-heading">Proceso Administrativo</div>
+            <div class="panel-body">
+            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Planeación (Objetivos, Estrategias, Programas).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Organización (Descripción de Puestos Claves, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Procesos y Procedimientos).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Integración (Presupuestos, Recursos).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Dirección (Mecanismos de Seguimiento).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Control (Balance Score Card).
+			</div>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+           <div class="panel panel-yellow">
+            <div class="panel-heading">Diagnóstico y Auditorías</div>
+            <div class="panel-body">
+            	<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Operativas.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Administrativas.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Aplicación de Indicadores del Desempeño.
+            </div>
+          </div>
+        </div>
+        
+        
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4">
+           <div class="panel panel-pink">
+            <div class="panel-heading">Capacitación Focalizada</div>
+            <div class="panel-body">
+            Adecuada a Necesidades, por Actualización en Puestos y/o Requerimientos Solicitados a la Empresa.
+ 
+			Estamos comprometidos con sus resultados.
+			Por ello desarrollamos y aplicamos las técnicas más efectivas del mercado en materia de cursos de capacitación y desarrollo de competencias.
+			</div>
+          </div>
+        </div>
+        
+        <div class="col-sm-4">
+           <div class="panel panel-yellow">
+            <div class="panel-heading">Administración Financiera</div>
+            <div class="panel-body">
+            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Definición de las estrategias de compra, venta, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;fusión, escisión, etc.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Determinación de rangos de valor.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Determinación precios de compra - venta.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Definición de estrategias de negociación.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Actuación como intermediarios entre las partes &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;involucradas.
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+           <div class="panel panel-blue">
+            <div class="panel-heading">Cursos de Capacitación</div>
+            <div class="panel-body">
+            Es por eso que en NOKIN, ofrecemos ; certificaciones en administración y desarrollo organizacional.
+			</div>
+          </div>
+        </div>
+    </div>
+</div>
+        
+</article>
+
+<article class="contenservdos">
+        
+        <div class="container">
+    <div class="row">
+
+    <h1 style="width: 100%;  margin-top: 5%; color: #3B3B3A; border-bottom: 1px black solid; padding-bottom: 1%;">Servicios <small>Consultoría en Computación</small></h1> <br><br><br>
+        <div class="col-sm-4">
+           <div class="panel panel-pink">
+            <div class="panel-heading">Networking y Comunicaciones</div>
+            <div class="panel-body">
+            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Colaboración. <br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Redes LAN, WAN.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Comunicaciones unificadas.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Videoconferencias y Tele presencia.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Redes empresariales(SW y ROUTER).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Centro de Datos y Virtualización.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Soluciones de almacenamiento.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Seguridad informática.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Redes Inalámbricas.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Equipos ópticos.
+			</div>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+           <div class="panel panel-yellow">
+            <div class="panel-heading">Infraestructura y Conectividad</div>
+            <div class="panel-body">
+            	<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura en Energización de equipos de &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Telecomunicaciones.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura de Cableado estructurado ( Voz, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Datos y Video).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura en Fibra óptica(Planta Externa/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Interna,).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura en Adecuación de Cuarto de &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Telecomunicaciones(site).<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Infraestructura en CCTV.
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+           <div class="panel panel-blue">
+            <div class="panel-heading">Estrategia y Planeación Tecnológica</div>
+            <div class="panel-body">
+            <i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Planteamiento de Escenarios Tecnológicos y de &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Inversión. <br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Asesoramiento sobre tecnologías.<br>
+			<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;&nbsp;Entendimiento de las necesidades del Negocio.<br>
+			</div>
+          </div>
+        </div>
+        
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4">
+           <div class="panel panel-pink">
+            <div class="panel-heading">Consultoría en Servicios</div>
+            <div class="panel-body">
+            Servicios de Consultoría Especializados en la mejora de los procesos organizacionales y de TIC bajo el respaldo profesional y del personal altamente calificado, certificado y con habilidades y conocimientos necesarios para cumplir el objetivo y la misión empresarial y/o institucional.
+			</div>
+          </div>
+        </div>
+        
+        <div class="col-sm-4">
+           <div class="panel panel-yellow">
+            <div class="panel-heading">Fabrica de Software y Seguridad Aplicativa</div>
+            <div class="panel-body">
+            	Área de especialización de desarrollo de soluciones de sistemas de información con respaldo profesional y del personal altamente calificado, certificado, con conocimientos y experiencia necesarios para llevar acabo el objetivo y la misión empresarial y/o institucional.
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+           <div class="panel panel-blue">
+            <div class="panel-heading">Cursos de Capacitación</div>
+            <div class="panel-body">
+            Es por eso que en NOKIN, ofrecemos ; certificaciones en seguridad y soporte.
+			</div>
+          </div>
+        </div>
+    </div>
+</div>
+<br><br>
+        <div style="text-align: center; width: 100%;">
+  <a href="contacto.php"><button type="button" class="btn btn-danger btn-lg">Contáctanos</button></a>
+</div>
+<br><br><br>
+</article>
     
 
 <span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
